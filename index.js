5e8e342e35cfa205e5ab64f9410e051a2348139b
Sergey <skurdin@yahoo.com>
2022-12-16 16:45:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,89 @@
-const { Walker, DepType } = require("flora-colossus");
-const { dirname } = require("path");
+import { Walker, DepType } from 'flora-colossus'
+import { dirname } from 'path'
 
 const defaultOpts = {
-  externals: [],
-  includeDeps: true,
-};
+  externals: [] as string[],
+  includeDeps: true
+}
 
 class ForgeExternalsPlugin {
-  __isElectronForgePlugin = true;
+  __isElectronForgePlugin = true
+  private _includeDeps: boolean
+  private _externals: string[]
+  private _dir: string
 
-  constructor(opts) {
-    const options = { ...defaultOpts, ...(opts || {}) };
-    this._externals = options.externals;
-    this._includeDeps = options.includeDeps;
+  constructor(opts: Record<string, unknown>) {
+    const options = { ...defaultOpts, ...(opts || {}) }
+    this._externals = options.externals
+    this._includeDeps = options.includeDeps
   }
 
-  init = (dir) => {
-    this._dir = dir;
-  };
+  init = (dir: string) => {
+    this._dir = dir
+  }
 
-  getHook(hookName) {
-    switch (hookName) {
-      case "resolveForgeConfig":
-        return this.resolveForgeConfig;
+  getHooks() {
+    return {
+      resolveForgeConfig: [this.resolveForgeConfig]
     }
   }
 
-  resolveForgeConfig = async (forgeConfig) => {
-    const foundModules = new Set(this._externals);
+  resolveForgeConfig = async (forgeConfig: {
+    packagerConfig: { ignore: (file: string) => boolean }
+  }) => {
+    const foundModules = new Set(this._externals) as Set<string>
 
     if (this._includeDeps) {
       for (const external of this._externals) {
         const moduleRoot = dirname(
           require.resolve(`${external}/package.json`, { paths: [this._dir] })
-        );
+        )
 
-        const walker = new Walker(moduleRoot);
+        const walker = new Walker(moduleRoot)
         // These are private so it's quite nasty!
-        walker.modules = [];
-        await walker.walkDependenciesForModule(moduleRoot, DepType.PROD);
+        // @ts-expect-error
+        walker.modules = []
+        // @ts-expect-error
+        await walker.walkDependenciesForModule(moduleRoot, DepType.PROD)
+        // @ts-expect-error
         walker.modules
-          .filter((dep) => dep.nativeModuleType === DepType.PROD)
-          .map((dep) => dep.name)
-          .forEach((name) => foundModules.add(name));
+          .filter(
+            (dep: { nativeModuleType: unknown }) => dep.nativeModuleType === DepType.PROD
+          )
+          .map((dep: { name: string }) => dep.name)
+          .forEach((name: string) => foundModules.add(name))
       }
     }
 
     // The webpack plugin already sets the ignore function.
-    const existingIgnoreFn = forgeConfig.packagerConfig.ignore;
+    const existingIgnoreFn = forgeConfig.packagerConfig.ignore
 
     // We override it and ensure we include external modules too
-    forgeConfig.packagerConfig.ignore = (file) => {
-      const existingResult = existingIgnoreFn(file);
+    forgeConfig.packagerConfig.ignore = (file: string) => {
+      const existingResult = existingIgnoreFn(file)
 
-      if (existingResult == false) {
-        return false;
+      if (existingResult === false) {
+        return false
       }
 
-      if (file === "/node_modules") {
-        return false;
+      if (file === '/node_modules') {
+        return false
       }
 
       for (const module of foundModules) {
-        if (file.startsWith(`/node_modules/${module}`) || file.startsWith(`/node_modules/${module.split('/')[0]}`)) {
-          return false;
+        if (
+          file.startsWith(`/node_modules/${module}`) ||
+          file.startsWith(`/node_modules/${module.split('/')[0]}`)
+        ) {
+          return false
         }
       }
 
-      return true;
-    };
+      return true
+    }
 
-    return forgeConfig;
-  };
+    return forgeConfig
+  }
 }
 
-module.exports = ForgeExternalsPlugin;
+export { ForgeExternalsPlugin }
