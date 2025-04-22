c34c3ee145651c24a6d77262200f6604c8c15699
Le An Nguyen <65287357+nguyenlean96@users.noreply.github.com>
2025-04-22 03:26:17 -0400

    feat: Migrate to use the webpack plugin constructor (#3)
    This commit migrates the forge.config.js file to use the webpack plugin constructor.
    This change allows the webpack plugin to be instantiated directly, which simplifies the configuration and improves readability.

--- tests/forge.config.js
+++ tests/forge.config.js
@@ -1,46 +1,44 @@
-const ForgeExternalsPlugin = require("..");
+const { default: WebpackPlugin } = require('@electron-forge/plugin-webpack');
+const { ForgeExternalsPlugin } = require('..');
 
 module.exports = {
-  packagerConfig: {},
-  makers: [
-    {
-      name: "@electron-forge/maker-squirrel",
-      config: {
-        name: "test_app",
-      },
-    },
-    {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {},
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
-    },
-  ],
-  plugins: [
-    [
-      "@electron-forge/plugin-webpack",
-      {
-        mainConfig: "./webpack.main.config.js",
-        renderer: {
-          config: "./webpack.renderer.config.js",
-          entryPoints: [
-            {
-              html: "./src/index.html",
-              js: "./src/renderer.ts",
-              name: "main_window",
-            },
-          ],
-        },
-      },
-    ],
-    new ForgeExternalsPlugin({
-      externals: ["native-hello-world"],
-    }),
-  ],
+	packagerConfig: {},
+	makers: [
+		{
+			name: '@electron-forge/maker-squirrel',
+			config: {
+				name: 'test_app',
+			},
+		},
+		{
+			name: '@electron-forge/maker-zip',
+			platforms: ['darwin'],
+		},
+		{
+			name: '@electron-forge/maker-deb',
+			config: {},
+		},
+		{
+			name: '@electron-forge/maker-rpm',
+			config: {},
+		},
+	],
+	plugins: [
+		new WebpackPlugin({
+			mainConfig: './webpack.main.config.js',
+			renderer: {
+				config: './webpack.renderer.config.js',
+				entryPoints: [
+					{
+						html: './src/index.html',
+						js: './src/renderer.ts',
+						name: 'main_window',
+					},
+				],
+			},
+		}),
+		new ForgeExternalsPlugin({
+			externals: ['native-hello-world'],
+		}),
+	],
 };
